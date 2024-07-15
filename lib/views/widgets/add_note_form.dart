8b88ffad1e87e222c2b3e15cfc26f7c67a467458
Youssef-Mustafa <youssef2.mostafa.1@gmail.com>
2024-07-15 13:31:37 +0300

    add addNotecubit to add note button sheet

---
+++ lib/views/widgets/add_note_form.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/add_note_button.dart';
+import 'package:notes_app/views/widgets/custom_text_field.dart';
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  String? title;
+  String? content;
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 40,
+          ),
+          CustomTextField(
+            onSave: (value) {
+              title = value;
+            },
+            hintText: 'Title',
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          CustomTextField(
+            maxLines: 5,
+            hintText: 'Content',
+            onSave: (value) {
+              content = value;
+            },
+          ),
+          const SizedBox(
+            height: 100,
+          ),
+          AddNoteButton(
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+        ],
+      ),
+    );
+  }
+}

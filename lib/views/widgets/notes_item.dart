01b8a14d43a5535f0653b99cbee8a91c4f1cefd1
Youssef-Mustafa <youssef2.mostafa.1@gmail.com>
2024-07-15 18:36:30 +0300

    Delete note done

--- lib/views/widgets/notes_item.dart
+++ lib/views/widgets/notes_item.dart
@@ -52,7 +52,9 @@ class NotesItem extends StatelessWidget {
                   ),
                 ),
                 trailing: IconButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    note.delete();
+                  },
                   icon: const Icon(
                     FontAwesomeIcons.trash,
                     color: Colors.black,
